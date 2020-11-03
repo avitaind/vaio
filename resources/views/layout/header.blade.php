ffe904a1009339f4570670e76f0503042322f72d
avitaind <52917489+avitaind@users.noreply.github.com>
2020-11-03 20:43:07 +0530

    Update header.blade.php

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -88,7 +88,7 @@
                             </li>
                              --->
                             <li class="nav-item text-center">
-                                <a class="nav-link px-md-4 py-2" href="{{ route('product.overview_se14_whl') }}">@lang('common.nav.product.vaio_se14_whl')</a>
+                                <a class="nav-link px-md-4 py-2" href="{{ route('product.overview_se14_whl') }}">@lang('common.nav.product.vaio_se14')</a>
                             </li>
                                <!---
                           <li class="nav-item text-center">
