84f34fbb73c1b775d8b60be2d77e4b3b50681a5f
AVITA India <avitaind@gmail.com>
2021-01-14 23:35:19 +0530

    website changes

--- resources/views/products/e15/spec.blade.php
+++ resources/views/products/e15/spec.blade.php
@@ -80,7 +80,7 @@
                     </li>
                     <li class="spec-item d-flex">
                         <div class="offset-md-1 col-4 col-md-3 spec-item-name">Storage</div>
-                        <div class="offset-md-1 spec-item-content">256GB - 512GB PCIe SSD</div>
+                        <div class="offset-md-1 spec-item-content"> 512GB PCIe SSD</div>
                     </li>
                     <li class="spec-item d-flex">
                         <div class="offset-md-1 col-4 col-md-3 spec-item-name">Graphics</div>
