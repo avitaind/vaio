19cd8662d63104ff15da1d1a798f9b01820ad5f6
avitaind <52917489+avitaind@users.noreply.github.com>
2021-01-20 23:06:22 +0530

    Update spec.blade.php

--- resources/views/products/e15/spec.blade.php
+++ resources/views/products/e15/spec.blade.php
@@ -12,7 +12,7 @@
 <div class="navbar-product-page d-lg-flex justify-content-lg-end" style="border-top: 1px solid #fff; border-bottom: 1px solid #fff;">
     <ul class="navbar-nav flex-wrap flex-row col col-lg-4 p-0">
 
-      <li class="nav-item text-center" style="flex-grow: 1; border-left: 1px solid white;">
+     <li class="nav-item text-center" style="flex-grow: 1; border-left: 1px solid white;">
             <a href="{{ route('product.overview_e15') }}" class="nav-link py-3 py-lg-2">@lang('common.overview')</a>
         </li>
         <li class="nav-item text-center" style="flex-grow: 1; border-left: 1px solid white;">
