21c53b9577389713c3f90df2b59302c18ad708ae
avitaind <52917489+avitaind@users.noreply.github.com>
2021-01-20 23:09:43 +0530

    Update gallery.blade.php

--- resources/views/products/e15/gallery.blade.php
+++ resources/views/products/e15/gallery.blade.php
@@ -13,8 +13,8 @@
 <div class="navbar-product-page d-lg-flex justify-content-lg-end" style="border-top: 1px solid #fff; border-bottom: 1px solid #fff;">
     <ul class="navbar-nav flex-wrap flex-row col col-lg-4 p-0">
 
-        <li class="nav-item text-center" style="flex-grow: 1; border-left: 1px solid white;">
-            <a href="{{ route('product.overview') }}" class="nav-link py-3 py-lg-2">@lang('common.overview')</a>
+         <li class="nav-item text-center" style="flex-grow: 1; border-left: 1px solid white;">
+            <a href="{{ route('product.overview_e15') }}" class="nav-link py-3 py-lg-2">@lang('common.overview')</a>
         </li>
         <li class="nav-item text-center" style="flex-grow: 1; border-left: 1px solid white;">
             <a href="{{ route('product.spec') }}" class="nav-link py-3 py-lg-2">@lang('common.spec')</a>
@@ -22,6 +22,7 @@
         <li class="nav-item text-center" style="flex-grow: 1; border-left: 1px solid white;">
             <a href="{{ route('product.gallery_e15') }}" class="nav-link py-3 py-lg-2">@lang('common.gallery')</a>
         </li>
+
         @if( $store_enabled )
             <li class="nav-item-preorder nav-item text-center" style="flex-grow: 4; border-left: 1px solid white;">
                 <a href="{{ $top_store_url }}" target="_blank" class="nav-link py-3 py-lg-2">@lang('common.buy_now')</a>
