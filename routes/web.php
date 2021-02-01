176d0c4d29590dc4992783ae7c6b65eaf4632438
AVITA India <avitaind@gmail.com>
2021-02-01 13:34:13 +0530

    coming soon url change

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::group([
     
 
     ///
-    Route::get('/product/fuji-z/overview', 'ProductController@getProductOverviewfuji')->name('product.overview_fuji-z');
+    Route::get('/product/comingsoon/overview', 'ProductController@getProductOverviewfuji')->name('product.overview_fuji-z');
 
     //e15
     Route::get('/product/e15/overview', 'ProductController@getProductOverview15')->name('product.overview_e15');
