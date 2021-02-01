736c8831c3f6f1072ea5fb4a90e81c64c61a2cd8
AVITA India <avitaind@gmail.com>
2021-02-01 11:40:43 +0530

    login redirect

--- resources/views/products/fuji-z/overview.blade.php
+++ resources/views/products/fuji-z/overview.blade.php
@@ -25,8 +25,46 @@
 </section>
 
 
+
 <!--- Banner end here------------------------------------>
 
+@push('css')
+   
+<style>
+		
+		img.map, map area{
+			outline: none;
+		}
+        
+		main{
+			background-color: #000; 
+		}
+		
+		.main-section img{
+			width: 100%; 
+			margin: auto;
+		}
+		
+       .mb_only {
+				display:none;
+        }
+       .pc_only {
+				display:block;
+        }
+
+        @media(max-width: 991px) {
+		 
+            .pc_only {
+				display:none;
+            }
+            .mb_only {
+				display:block;
+            }
+        }
+
+    </style>
+
+@endpush
 
 <!---Page content end here--------------------------------------------->
 
@@ -34,6 +72,7 @@
 
 @section('css')
     <link type="text/css" rel="stylesheet" href="{{ asset('css/product.css') }}" />
+ 
 @stop
 
 @section('js')
