b5eb6c88104b684245235dd8fff7f34f256addd8
avitaind <avitaind@gmail.com>
2021-03-04 17:55:59 +0530

    sx14 coming soon

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -35,7 +35,7 @@
                                 <a class="nav-link px-md-4 py-2" href="{{ route('product.overview_se14') }}"> @lang('common.nav.product.vaio_s14')</a>
                             </li>
                             <li class="nav-item text-center">
-                                <a class="nav-link px-md-4 py-2" > SX14 (Coming Soon)</a>
+                                <a class="nav-link px-md-4 py-2" > SX14 (COMING SOON)</a>
                             </li>
                            <!---
                             <li class="nav-item text-center">
