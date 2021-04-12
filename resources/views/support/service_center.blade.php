c96f4009e209f93e5479506e943795e9d6687761
avitaind <avitaind@gmail.com>
2021-04-12 13:52:22 +0530

    covid disclimare

--- resources/views/support/service_center.blade.php
+++ resources/views/support/service_center.blade.php
@@ -42,6 +42,11 @@
               
 ----->
 
+            <div class="container">
+                    <div class="alert alert-dismissible alert-info">
+                    Due to the global outbreak of the coronavirus (COVID-19), the service center will adjust the operation time or temporarily shut down for the time being. The adjustment will be subjected to local regulations and will be updated as soon as possible. We thank you for your patience and understanding.
+                    </div>
+            </div>
                 <div class="col-12 page-title mb-3 ls-0">@lang('common.contact_us.service_support.title')</div>
                   	<div class="shopsList">
 
