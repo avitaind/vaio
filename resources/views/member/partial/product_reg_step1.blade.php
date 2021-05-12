56f27450c85096010cc2b98eb6b02d7c9b4b72fe
GOD OF THUNDER <avitaind@gmail.com>
2021-05-12 18:39:19 +0530

    product registration

--- resources/views/member/partial/product_reg_step1.blade.php
+++ resources/views/member/partial/product_reg_step1.blade.php
@@ -54,14 +54,11 @@
 
     <div class="form-group row">
             <div class="col-12 col-lg-10 col-xl-9">
-            <!-- <button type="button" style="float: right;" class="btn btn-primary  my-3 py-3 btn btn-primary next-step next-button">
+            <button type="button" style="float: right;" class="btn btn-primary  my-3 py-3 btn btn-primary next-step next-button">
                 @lang('member.form_next')
-            </button> -->
+            </button>
 
          
-                            <a href="#step2" data-toggle="tab" aria-controls="step2" role="tab" title="@lang('member.step2')" class="nav-link disabled">
-                                <span class="round-tab"><div>NEXT</div></span>
-                            </a>
                        
     </div>
     </div>
