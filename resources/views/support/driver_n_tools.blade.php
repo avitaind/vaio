4f16b6ca591bb3364d1ea7106ba28ed095ec802c
GOD OF THUNDER <avitaind@gmail.com>
2021-05-24 11:13:28 +0530

    warrnaty link

--- resources/views/support/driver_n_tools.blade.php
+++ resources/views/support/driver_n_tools.blade.php
@@ -15,7 +15,7 @@
                             Thanks for the valuable suggestion from our customers. You can purchase the newly launched “Warranty Extension Service” online now. You may choose to extend 1 or 2 years to enjoy our worry-free warranty service.
                             </div>                           
                             <div class="text-center purchase_steps">
-                                <a href="/support#support-warranty" class="btn btn-primary support-search-btn-block mt-3 mt-md-0 mx-auto ml-md-3 px-5 px-md-0">
+                                <a href="https://hk.vaio.com/en/warranty-extension" class="btn btn-primary support-search-btn-block mt-3 mt-md-0 mx-auto ml-md-3 px-5 px-md-0">
                                     See More
                                 </a> 
                             </div>  
