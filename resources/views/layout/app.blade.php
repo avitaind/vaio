d41d82b0faa7433d51abd112729cce8669c067ee
GOD OF THUNDER <avitaind@gmail.com>
2021-07-02 16:45:57 +0530

    GA code refresh

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -97,7 +97,7 @@
 
 
     <!-- Google Analytics -->
-    <script>
+    <!-- <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -105,7 +105,7 @@
 
         ga('create', 'UA-119970250-5', 'auto');
         ga('send', 'pageview');
-    </script>
+    </script> -->
     <!-- End Google Analytics -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
