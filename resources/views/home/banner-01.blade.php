0815c71bf7fb91a972414da1745258000a2d63d3
avitaind <avitaind@gmail.com>
2021-10-05 13:35:13 +0530

    home banner v1

---
+++ resources/views/home/banner-01.blade.php
@@ -0,0 +1,16 @@
+<section class="main-banner-section" style="height: fit-content;">
+    <div class="swiper-container swiper-container-horizontal">
+        <div class="swiper-wrapper">
+               <a href="#" target="_blank" class="swiper-slide">
+                        <div class="main-banner-wrapper cms-banner" style="background-image: url('/images/banner/VAIO_Z_DESKTPOP.jpg')" >
+                        </div>
+                    </a>
+
+
+
+
+
+
+        </div>
+    </div>
+</section>
