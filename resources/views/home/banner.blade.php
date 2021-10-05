b1752ab678e29b60372f802f703c4c665c65624c
avitaind <avitaind@gmail.com>
2021-10-05 11:25:54 +0530

    swiper pagination hide

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,17 +1,17 @@
-<section class="main-banner-section" style="height: fit-content; position: relative;">
+<section class="main-banner-section" style="height: fit-content;">
     <div class="swiper-container swiper-container-horizontal">
         <div class="swiper-wrapper">
 
-	  @foreach( $banners as $index => $banner )
+            @foreach( $banners as $index => $banner )
 
-              @if( $banner->url )
+                @if( $banner->url )
                     @php
                         $url_host = parse_url($banner->url, PHP_URL_HOST);
 
                         if ( $url_host == null ) {
                             $is_same_host = true;
 
-                       } else {
+                        } else {
                             $is_same_host = ( $url_host == request()->getHost() );
                         }
 
@@ -23,43 +23,35 @@
                         </div>
                     </a>
                 @else
-
-                  <div class="swiper-slide">
+                    <div class="swiper-slide">
                         <div class="main-banner-wrapper cms-banner cms-banner-{{ $index }}">
                             <div class="main-banner" title=""></div>
                         </div>
                     </div>
-
                 @endif
+
+
 
             @endforeach
 
         </div>
-
-        <!-- <div class="swiper-pagination"></div> -->
-
+        <div class="swiper-pagination"></div>
     </div>
-
 </section>
 
 
 @push('css')
 
     <style>
-
-
         .cms-banner:before {
             padding-top: 37.51%;
-
         }
 
         @media( max-width: 991px) {
             .cms-banner:before {
                 padding-top: 106%;
             }
-
-    }
-
+        }
 
 
         @foreach( $banners as $index => $banner )
@@ -72,6 +64,8 @@
                 background-image: url( {{ $banner->mobile_banner_url }} );
             }
         }
+
+
         @endforeach
     </style>
 
