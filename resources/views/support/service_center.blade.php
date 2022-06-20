0d7cca0f3671fcb9b1e03c65cf345afea7b67e44
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-06-20 11:05:26 +0530

    support page hide section

--- resources/views/support/service_center.blade.php
+++ resources/views/support/service_center.blade.php
@@ -1,5 +1,5 @@
 @php
-    $centers = \App\NewServiceCenter::orderBy('id', 'asc')->paginate(10);
+$centers = \App\NewServiceCenter::orderBy('id', 'asc')->paginate(10);
 @endphp
 
 <div id="support-center" class="support-center-wrap">
@@ -8,26 +8,26 @@
 
 
 
-        <div class="container">
-            <div class="mt-5 pt-0 pt-lg-4">
-               <!--- <h2 class="page-title mb-5">@lang('common.contact_us.headquarter.title')</h2> --->
-                <div class="row no-gutters my-4 col-12 mx-auto ml-0 ml-md-4">
+            <div class="container">
+                <div class="mt-5 pt-0 pt-lg-4">
+                    <!--- <h2 class="page-title mb-5">@lang('common.contact_us.headquarter.title')</h2> --->
+                    <div class="row no-gutters my-4 col-12 mx-auto ml-0 ml-md-4">
 
-                    <div class="col-12 col-md-4 text-center mb-4 mb-lg-0">
-                        <img class="info-icon" src="/images/icons/phone.png">
-                        <p class="content-text mt-3 mb-0">@lang('common.contact_us.headquarter.general_phone')</p>
-                        <a href="tel:18001039635" class="sub-text">0120-4761661</a>
-                    </div>
+                        <div class="col-12 col-md-4 text-center mb-4 mb-lg-0">
+                            <img class="info-icon" src="/images/icons/phone.png">
+                            <p class="content-text mt-3 mb-0">@lang('common.contact_us.headquarter.general_phone')</p>
+                            <a href="tel:18001039635" class="sub-text">0120-4761661</a>
+                        </div>
 
-                    <div class="col-12 col-md-4 text-center mb-4 mb-lg-0">
-                        <img class="info-icon" src="/images/icons/email-icon.png">
-                        <p class="content-text mt-3 mb-0 ">@lang('common.contact_us.headquarter.general')</p>
-                        <a href="mailto:Supportin@in.vaio.com" class="sub-text">Supportin@in.vaio.com</a>
+                        <div class="col-12 col-md-4 text-center mb-4 mb-lg-0">
+                            <img class="info-icon" src="/images/icons/email-icon.png">
+                            <p class="content-text mt-3 mb-0 ">@lang('common.contact_us.headquarter.general')</p>
+                            <a href="mailto:Supportin@in.vaio.com" class="sub-text">Supportin@in.vaio.com</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-<!---
+                <!---
                 <div class="col-12 col-md-4 text-center mb-4 mb-lg-0">
                     <img class="info-icon" src="/images/icons/phone.png">
                     <p class="content-text mt-3 mb-0">@lang('common.contact_us.headquarter.general_phone')</p>
@@ -42,14 +42,17 @@
 
 ----->
 
-            <div class="container">
+                <div class="container">
                     <div class="alert alert-dismissible alert-info">
-                    Due to the global outbreak of the coronavirus (COVID-19), the service center will adjust the operation time or temporarily shut down for the time being. The adjustment will be subjected to local regulations and will be updated as soon as possible. We thank you for your patience and understanding.
+                        Due to the global outbreak of the coronavirus (COVID-19), the service center will adjust the
+                        operation time or temporarily shut down for the time being. The adjustment will be subjected to
+                        local regulations and will be updated as soon as possible. We thank you for your patience and
+                        understanding.
                     </div>
-            </div>
-                <div class="col-12 page-title mb-3 ls-0">@lang('common.contact_us.service_support.title')</div>
+                </div>
+                {{-- <div class="col-12 page-title mb-3 ls-0">@lang('common.contact_us.service_support.title')</div> --}}
 
-                <div class="form-group">
+                {{-- <div class="form-group">
 
                     <div class="input-group">
 
@@ -57,12 +60,12 @@
                     <div class="input-group-addon overlay-addon"><i class="fa fa-search" aria-hidden="true"></i></div>
 
                     </div>
-                </div>
+                </div> --}}
 
-                  	<div class="shopsList" id="shopsList">
-                        @if(!empty($centers) && $centers->count())
-                        @foreach( $centers as $search )
-                     <div class="col-md-6">
+              <!--  <div class="shopsList" id="shopsList">
+                    @if (!empty($centers) && $centers->count())
+                        @foreach ($centers as $search)
+                            <div class="col-md-6">
                                 <div>
                                     <span class="address">{{ $search->address }}</span>
                                 </div>
@@ -76,14 +79,14 @@
                                     <a class="tel" href="tel:{{ $search->phone }}">{{ $search->phone }}</a>
                                 </div>
                                 <br><br>
-                         </div>
-                     @endforeach
-                     @else
-                     <tr>
-                         <td colspan="10">There are no data.</td>
-                     </tr>
-                 @endif
-                            {{-- @for( $i = 0 ; $i < count($centers) ; $i++ )
+                            </div>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="10">There are no data.</td>
+                        </tr>
+                    @endif
+                    {{-- @for ($i = 0; $i < count($centers); $i++)
 
                                 @php
                                     $center = $centers[$i];
@@ -101,24 +104,24 @@
                             @endfor --}}
 
 
-                    </div><!--shopsList-->
+                </div> -->
+                <!--shopsList-->
 
             </div>
         </div>
-        {!! $centers->links() !!}
+        {{-- {!! $centers->links() !!} --}}
     </div>
 </div>
 
 
 @push('css')
-
     <style>
         /*#support-center-list {*/
 
-            /*position: absolute;*/
-            /*left: .5em;*/
-            /*right: .5em;*/
-            /*z-index: 1;*/
+        /*position: absolute;*/
+        /*left: .5em;*/
+        /*right: .5em;*/
+        /*z-index: 1;*/
         /*}*/
 
         #support-center-list .list-group-item {
@@ -128,7 +131,7 @@
         }
 
         #tw_info {
-            background-color:white;
+            background-color: white;
             height: 600px;
             overflow-y: scroll;
         }
@@ -152,21 +155,16 @@
             left: 0px;
             top: 5px;
         }
-
     </style>
-
 @endpush
 
 @push('js')
-
     <script>
-
-
         var shops = @json($centers);
 
         var selected_shop_index = 0;
 
-        function update_center_info ( center , scrollToSection ) {
+        function update_center_info(center, scrollToSection) {
 
 
             // $("#center_address").text( center.title );
@@ -177,12 +175,12 @@
 
             // Update Center Pin.
 
-            if ( maps ) {
+            if (maps) {
                 var location = new google.maps.LatLng(center.latitude, center.longitude);
                 markers.setPosition(location);
                 maps.setCenter(location);
 
-                if ( scrollToSection ) {
+                if (scrollToSection) {
                     $('html, body').animate({
                         scrollTop: $("#support-center").offset().top - 72
                     });
@@ -192,15 +190,15 @@
         }
 
 
-        function hideCenterPicker( ) {
+        function hideCenterPicker() {
 
             var list = $("#support-center-list");
 
-            if ( list.hasClass('collapsing')) {
+            if (list.hasClass('collapsing')) {
                 return; // Do nothing.
             }
 
-            if ( list.hasClass('show') ) {
+            if (list.hasClass('show')) {
                 list.collapse('hide');
             }
         }
@@ -217,63 +215,55 @@
 
 
 
-            $("#support-center-list .shop-info").click(function( element ) {
+            $("#support-center-list .shop-info").click(function(element) {
 
                 // hideCenterPicker();
-    console.log("SHOP CLICK!");
+                console.log("SHOP CLICK!");
 
                 var index = $(this).data('shop-index');
                 selected_shop_index = index;
-                update_center_info( shops[index] , true);
+                update_center_info(shops[index], true);
             });
 
-            update_center_info( shops[0] , false );
+            update_center_info(shops[0], false);
 
         });
-
-
-
-
-
-
     </script>
 
-<script type="text/javascript">
-
-    $('#search').on('keyup', function(){
-            $value=$(this).val();
+    <script type="text/javascript">
+        $('#search').on('keyup', function() {
+            $value = $(this).val();
             $.ajax({
                 type: 'get',
-                url: '{{URL::to('search')}}',
-                data: { 'search': $value},
-                success:function(data){
+                url: '{{ URL::to('search') }}',
+                data: {
+                    'search': $value
+                },
+                success: function(data) {
                     $('#shopsList').html(data);
                 }
             })
         })
-
     </script>
 
 
-<script type="text/javascript" src="{{ asset('js/support.js') }}"></script><script type="text/javascript">
-
-            $('#search').on('keyup', function(){
-                    $value=$(this).val();
-                    $.ajax({
-                        type: 'get',
-                        url: '{{URL::to('search')}}',
-                        data: { 'search': $value},
-                        success:function(data){
-                            $('#shopsList').html(data);
-                        }
-                    })
-                })
-
-            </script>
+    <script type="text/javascript" src="{{ asset('js/support.js') }}"></script>
+    <script type="text/javascript">
+        $('#search').on('keyup', function() {
+            $value = $(this).val();
+            $.ajax({
+                type: 'get',
+                url: '{{ URL::to('search') }}',
+                data: {
+                    'search': $value
+                },
+                success: function(data) {
+                    $('#shopsList').html(data);
+                }
+            })
+        })
+    </script>
 
 
     <script type="text/javascript" src="{{ asset('js/support.js') }}"></script>
-
-
-
 @endpush
